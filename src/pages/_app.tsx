befae70612b543a173eec9264e58c59f0a504dc1
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-09-19 22:47:56 +0300

    installing dependencies and creating a folder structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/src/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { ReactElement, ReactNode, useState } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const [queryClient] = useState(() => new QueryClient())
+  const getLayout = Component.getLayout ?? (page => page)
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>{getLayout(<Component {...pageProps} />)}</Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
