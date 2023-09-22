f886c7d241dbb4dd837809cf1a6e259fde4b7063
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-09-23 00:39:26 +0300

    save changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,7 @@ module.exports = {
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/click-events-have-key-events': 0,
     'react/function-component-definition': 0,
+    'import/no-extraneous-dependencies': 0,
+    'global-require': 0,
   },
 };
