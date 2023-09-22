e086fd99c5a9c86412c61aca8c38aaa231a99042
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-09-22 23:31:10 +0300

    save changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Inter } from 'next/font/google'
-import {getGlobalLayout} from "@/components/layouts/Globalayout";
-import Head from 'next/head'
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import { getGlobalLayout } from '@/components/layouts/Globalayout';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export const Home = () => {
+export function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -15,11 +15,13 @@ export const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='flex flex-col bg-red-400 justify-center align-middle'><span className='text-emerald-900'>Content</span></div>
-      </main>
-  )
+      <div className="flex flex-col bg-red-400 justify-center align-middle">
+        <span className="text-emerald-900">Content</span>
+      </div>
+    </main>
+  );
 }
 
-Home.getLayout = getGlobalLayout
+Home.getLayout = getGlobalLayout;
 
-export default Home
+export default Home;
