b896d77252d6e15698da3e128ee54c5807302c2a
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-10-08 14:08:59 +0300

    ref(layoutsContainter) - changing the width of the container

---
+++ src/pages/account/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import { SignUp } from '@/modules/auth-modules/sign-up-module';
+
+export const Settings = () => {
+  return (
+    <>
+      <Head>
+        <title>Account</title>
+      </Head>
+      <div>asdfasd</div>
+    </>
+  );
+};
+export default Settings;
