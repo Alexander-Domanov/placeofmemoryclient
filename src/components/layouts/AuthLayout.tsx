21cbbc311d99f545fb62c7cf32dd15f3da997d6a
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-10-11 21:53:26 +0300

    ref(signup) - added another video for the registration page

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -3,9 +3,16 @@ import { Container } from '@/components';
 
 interface AuthLayoutProps {
   children: ReactNode;
+  videoSrc?: string;
 }
 
-export const AuthLayout = ({ children }: AuthLayoutProps) => {
+const baseVideoSrc =
+  'https://media.istockphoto.com/id/1406990292/video/bright-sunlight-breaks-through-lush-grass-waving-in-wind.mp4?s=mp4-640x640-is&k=20&c=Wg69EHKzHPK8-KVau4NYV88A4Fs7nmLc_aqDiwJRPsM=';
+
+export const AuthLayout = ({
+  children,
+  videoSrc = baseVideoSrc,
+}: AuthLayoutProps) => {
   return (
     <Container types="auth">
       <section className="w-[450px] h-screen">
@@ -15,7 +22,7 @@ export const AuthLayout = ({ children }: AuthLayoutProps) => {
           autoPlay
           loop
           muted
-          src="https://media.istockphoto.com/id/1406990292/video/bright-sunlight-breaks-through-lush-grass-waving-in-wind.mp4?s=mp4-640x640-is&k=20&c=Wg69EHKzHPK8-KVau4NYV88A4Fs7nmLc_aqDiwJRPsM="
+          src={videoSrc}
         >
           Your browser does not support the video tag.
         </video>
