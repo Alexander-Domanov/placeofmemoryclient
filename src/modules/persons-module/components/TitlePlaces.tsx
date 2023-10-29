4bf7cc3d5a5e262c0cf96cdf0d9a60a3a2f140b7
Alexsandr3 <fam_86@mail.ru>
2023-10-29 13:26:27 +0100

    feat(places) added title form fot places

---
+++ src/modules/persons-module/components/TitlePlaces.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useState } from 'react';
+import { AutoComplete } from 'antd';
+import { useDebounce } from 'usehooks-ts';
+import { useTitlePlaces } from '@/modules/places-module/hooks/useTtitlePlaces';
+
+interface TitlePlacesFormProps {
+  onFinishValue: (id: number) => void;
+}
+export const TitlePlaces: FC<TitlePlacesFormProps> = ({ onFinishValue }) => {
+  const [pagination, setPagination] = useState({
+    page: 1,
+    pageSize: 3,
+    searchTerm: '',
+  });
+
+  const search = useDebounce(pagination.searchTerm, 500);
+
+  const { titlePlaces } = useTitlePlaces(
+    pagination.page,
+    pagination.pageSize,
+    search
+  );
+
+  const handleSearch = (value: string) => {
+    setPagination({ ...pagination, searchTerm: value });
+  };
+
+  const options = titlePlaces?.items.map((place) => ({
+    value: place.nameCemetery,
+    id: place.id,
+  }));
+
+  const findIdFromOption = (selectedValue: string) => {
+    const selectedOption = options?.find(
+      (option) => option.value === selectedValue
+    );
+
+    if (selectedOption) {
+      return selectedOption.id;
+    }
+    return null;
+  };
+
+  return (
+    <AutoComplete
+      style={{ width: '100%' }}
+      onSearch={handleSearch}
+      placeholder="Search place by name"
+      options={options}
+      allowClear
+      onSelect={(value) => {
+        const id = findIdFromOption(value);
+        if (id !== null) {
+          onFinishValue(id);
+        }
+      }}
+    />
+  );
+};
