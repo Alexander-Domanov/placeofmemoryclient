d941327ae86e10f9ab3d9b27e79ddf70a79002b6
Alexsandr3 <fam_86@mail.ru>
2023-10-29 16:06:23 +0100

    feat(persons) changed mistake

--- src/modules/persons-module/components/Person.tsx
+++ src/modules/persons-module/components/Person.tsx
@@ -127,7 +127,7 @@ export const PersonPage: FC = () => {
       });
     } else {
       updatePersonMutation(
-        { id: person, person: newPerson },
+        { id: personId, person: newPerson },
         {
           onSuccess: () => {
             notification.success({
