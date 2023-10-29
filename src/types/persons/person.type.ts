ec0dee379a1975514bbdd5fcaeeafa2f07decb7f
Alexsandr3 <fam_86@mail.ru>
2023-10-29 13:24:13 +0100

    feat(persons) added types

--- src/types/persons/person.type.ts
+++ src/types/persons/person.type.ts
@@ -1,11 +1,13 @@
 import { ILocation } from '@/types/locations/location.type';
 import { IGalleryFile } from '@/types/images/gallery-file.type';
+import { IBaseOwnerInfo } from '@/types/common/owner-info.type';
+import { IBaseDateType } from '@/types';
 
-export interface IPerson extends ILocation {
+export interface IPerson extends ILocation, IBaseDateType, IBaseOwnerInfo {
   status: string;
   id: number;
-  ownerId: number;
-  fillName: string;
+  firstName: string;
+  lastName: string;
   birthDate: string;
   deathDate: string;
   description: string;
