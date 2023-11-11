1861c5fc926ec51fbdd1f9a08ffdda0b792884dd
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-11-11 14:39:36 +0300

    ref/places - changed bread crumbs and added a place for the map

--- src/modules/places-main-module/components/PlaceMain.tsx
+++ src/modules/places-main-module/components/PlaceMain.tsx
@@ -1,13 +1,11 @@
 import { useRouter } from 'next/router';
-import { IoEarthOutline } from 'react-icons/io5';
 import Link from 'next/link';
-import { GrLinkNext } from 'react-icons/gr';
-import { AiOutlineLeftCircle, AiFillHome } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineLeftCircle } from 'react-icons/ai';
 import { useGetPlaceMain } from '@/modules/places-main-module';
 import { ImageComponent } from '@/ui/image/ImageComponent';
 import { routes } from '@/common/routing/routes';
-import { Button } from '@/ui';
 import { useWindowSize } from '@/common/hooks/useWindowResize';
+import { MarkupRenderer } from '@/common/helpers/MarkupRenderer';
 
 export const PlaceMain = () => {
   const { query } = useRouter();
@@ -15,10 +13,6 @@ export const PlaceMain = () => {
   const { dataPlace } = useGetPlaceMain({ slug });
   const { width } = useWindowSize();
 
-  // eslint-disable-next-line react/no-unstable-nested-components
-  const MarkupRenderer = ({ markup }: { markup: string }) => {
-    return <div dangerouslySetInnerHTML={{ __html: markup }} />;
-  };
   const prevPageLabel = (
     <div className="flex mt-[50px]">
       <Link href={routes.places.index}>
@@ -38,7 +32,7 @@ export const PlaceMain = () => {
           <div className="flex flex-col">
             <div className="flex items-center sm:text-xs sm:justify-center md:justify-center lg:justify-center   text-dark-100 gap-3 mb-5 text-xl leading-[64px] font-light">
               <Link href={routes.main} className="cursor-pointer">
-                <AiFillHome size={24} />
+                <AiOutlineHome className="text-dark-100" size={22} />
               </Link>
               /
               <Link
@@ -60,9 +54,9 @@ export const PlaceMain = () => {
           <div className="flex mt-[100px] lg:justify-center lg:flex-wrap gap-[100px]">
             <div className="flex flex-col gap-10">
               <div className="flex flex-col gap-3">
-                <h2 className="leading-6 sm:text-2xl break-words text-light-300 font-bold sm:leading-3 text-5xl">
+                <h1 className="leading-6 sm:text-2xl break-words text-light-300 font-bold sm:leading-3 text-5xl">
                   {dataPlace.nameCemetery}
-                </h2>
+                </h1>
               </div>
               <div className="flex lg:justify-center">
                 <div className="flex md:w-[200px] md:h-[250px] w-[450px] h-[500px] bg-dark-900">
@@ -80,7 +74,7 @@ export const PlaceMain = () => {
               </div>
             </div>
             <div className="flex flex-col gap-[70px]">
-              <div className="flex flex-col gap-3">
+              <div className="flex flex-col gap-7 md:gap-3 sm:gap-3">
                 <h2 className="text-5xl text-dark-100 font-light leading-7">
                   Месца знаходжання
                 </h2>
@@ -88,30 +82,18 @@ export const PlaceMain = () => {
                   {dataPlace.city}, {dataPlace.country}
                 </span>
               </div>
-              <div className="flex flex-col gap-7">
-                <h2 className="text-5xl font-light leading-7 text-dark-100">
+              <div className="flex flex-col gap-7 md:gap-3 sm:gap-3">
+                <h3 className="text-5xl font-light leading-7 text-dark-100">
                   Апісанне
-                </h2>
-                <div className="text-xl text-light-300 leading-7 break-words font-light">
+                </h3>
+                <section className="text-xl text-light-300 leading-7 break-words font-light">
                   <MarkupRenderer markup={dataPlace.description} />
-                </div>
-                <div className="flex gap-3 md:gap-4 items-center flex-wrap mt-[70px]">
-                  <div className="flex gap-3">
-                    <IoEarthOutline size={24} />
-                    <span className="text-light-300 text-xl font-light leading-7">
-                      45.44221740005173, 9.153927906369892
-                    </span>
-                  </div>
-                  <div className="flex justify-end">
-                    <Button
-                      variant="default"
-                      className="w-[200px] flex gap-4"
-                      // onClick={() => push(routes.ex)}
-                    >
-                      НА МАПЕ
-                      <GrLinkNext size={17} />
-                    </Button>
-                  </div>
+                </section>
+                <div className="flex gap-7 md:gap-3 sm:gap-3 flex-col mt-[70px]">
+                  <h3 className="text-5xl font-light leading-7 text-dark-100">
+                    На мапе
+                  </h3>
+                  <section className="">Карта</section>
                 </div>
               </div>
             </div>
