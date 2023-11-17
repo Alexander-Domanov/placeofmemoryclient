1b33eb2765b3689c57d054d2adafeb43d20a1e01
Alexsandr3 <fam_86@mail.ru>
2023-11-17 11:51:06 +0100

    fix(dashboard) added slug roles

--- src/modules/articles-module/constants/ArticleFormRules.tsx
+++ src/modules/articles-module/constants/ArticleFormRules.tsx
@@ -1,15 +1,23 @@
 export const ArticleFormRules = {
   title: [
     { required: true },
-    { max: 155, message: `Title must be at most 155 characters` },
+    { max: 155, message: `Title limit is 155 characters` },
   ],
   description: [
     { required: true },
-    { max: 355, message: `Description must be at most 355 characters` },
+    { max: 355, message: `Description limit is 355 characters` },
   ],
   content: [
     { required: true },
-    { max: 10000, message: `Content must be at most 10 000 characters` },
+    { max: 10000, message: `Content limit is 10000 characters` },
+  ],
+  slug: [
+    { required: true },
+    { max: 120, message: `Slug limit is 120 characters` },
+    {
+      pattern: /^[a-z0-9]+(?:-[a-z0-9]+)*$/,
+      message: `Slug should be in lower case and separated by hyphens`,
+    },
   ],
   photo: {
     rules: [{ required: true }],
