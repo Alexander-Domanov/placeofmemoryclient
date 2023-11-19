4f608923cdd0094a7fda23416d8cc367b44b28c2
Alexsandr3 <fam_86@mail.ru>
2023-11-19 10:54:43 +0100

    feat(maps) changed style for main map

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,21 +1,42 @@
 import Head from 'next/head';
-import { getGlobalLayout } from '@/components';
+import { FC } from 'react';
+import { GetStaticProps } from 'next';
 import { MapsMain } from '@/modules/maps-module/components/maps';
 import { useTranslation } from '@/components/internationalization';
+import { IContacts, IGetArticlesResponse } from '@/types';
+import { SiteLayout } from '@/components/layouts/SiteLayout';
+import { getContacts } from '@/modules/contacts-module/api/contacts-api';
 
-const Map = () => {
+interface Props {
+  contacts: IContacts;
+  posts: IGetArticlesResponse;
+}
+
+const Map: FC<Props> = ({ contacts, posts }) => {
   const { t } = useTranslation();
   return (
     <>
       <Head>
-        <title>{t.map.indexTitle} | MOGILKI</title>
+        <title>{`${t.map.indexTitle} | MOGILKI`}</title>
       </Head>
 
-      <MapsMain />
+      <SiteLayout contacts={contacts}>
+        <MapsMain />
+      </SiteLayout>
     </>
   );
 };
 
-Map.getLayout = getGlobalLayout;
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { data: contacts } = await getContacts();
+
+  return {
+    props: {
+      contacts,
+    },
+    revalidate: 30,
+  };
+};
+// Map.getLayout = getGlobalLayout;
 
 export default Map;
