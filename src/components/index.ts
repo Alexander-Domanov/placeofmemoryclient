8ab3d77ba4e4b2a8c974ed9fd2d376209f435a5f
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-11-20 23:35:28 +0300

    ref - delete global layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 // layouts
 export * from './layouts/LayoutWithHeader';
-export * from './layouts/Globalayout';
 export { AuthLayout } from './layouts/AuthLayout';
 export { Header } from './layouts/components/Header';
 export { Container } from './layouts/components/Containter';
