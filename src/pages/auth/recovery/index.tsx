2e734dda92db696f8ef75349c222a3e377da5498
Alexander-Domanov <alexanderdomanov.dev@gmail.com>
2023-11-21 00:37:47 +0300

    ref/auth - footer removed on the recover page

--- src/pages/auth/recovery/index.tsx
+++ src/pages/auth/recovery/index.tsx
@@ -1,15 +1,34 @@
 import Head from 'next/head';
-import { NextPageWithLayout } from '@/pages/_app';
+import { GetStaticProps } from 'next';
 import { Recovery } from '@/modules/auth-modules/new-password-recovery-module';
 import { useTranslation } from '@/components/internationalization';
+import { getArticlesPublic } from '@/modules/articles-module/api/articles-api';
+import { getContacts } from '@/modules/contacts-module/api/contacts-api';
+import { IPageContacts } from '@/types';
+import { Header } from '@/components';
 
-const PageRecovery: NextPageWithLayout = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const { data: posts } = await getArticlesPublic();
+  const { data: contacts } = await getContacts();
+
+  return {
+    props: {
+      posts,
+      time: Date.now(),
+      contacts,
+    },
+    revalidate: 30,
+  };
+};
+const PageRecovery = ({ contacts }: IPageContacts) => {
   const { t } = useTranslation();
   return (
     <div>
       <Head>
         <title>{t.auth.recovery.indexTile} | Mogilki</title>
       </Head>
+
+      <Header />
       <Recovery />
     </div>
   );
