f0e32f82b0004f240242c8fd3e545ee6573ba31c
Alexsandr3 <fam_86@mail.ru>
2023-11-25 12:24:21 +0100

    feat(about) added internationalization

--- src/modules/about-module/About.tsx
+++ src/modules/about-module/About.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { IContacts } from '@/types';
 import BreadcrumbMain from '@/components/Breadcrumb/BreadcrumbMain';
 import { useTranslation } from '@/components/internationalization';
+import candle from '@/assets/images/home/candle.jpg';
+import { ImageComponent } from '@/ui/image/ImageComponent';
 
 interface Props {
   contacts: IContacts;
 }
 export const About: React.FC<Props> = ({ contacts }) => {
   const { t } = useTranslation();
-  const { title: titleT } = t.aboutTheProject.page;
+  const { title: titleT, description: descriptionT } = t.aboutTheProject.page;
   return (
     <div className="bg-dark-700 pt-[60px] pb-[60px] pl-[60px] pr-[60px] md:pt-[28px] md:pb-[28px]  lg:pl-[12px] lg:pr-[12px] md:pl-[4px] md:pr-[4px]">
       <div className="container">
@@ -21,10 +23,20 @@ export const About: React.FC<Props> = ({ contacts }) => {
         <div className="mt-6 h-[1px] bg-dark-300" />
 
         <div className="flex items-center justify-center mt-10">
-          <div
-            className="mt-4 wysiwyg sm:text-xs"
-            dangerouslySetInnerHTML={{ __html: contacts.about }}
-          />
+          {contacts?.about.length > 0 ? (
+            <div
+              className="mt-4 wysiwyg sm:text-xs"
+              dangerouslySetInnerHTML={{ __html: contacts.about }}
+            />
+          ) : (
+            <ImageComponent
+              alt="Candle picture"
+              width={730}
+              height={268}
+              className="rounded-lg"
+              src={candle}
+            />
+          )}
         </div>
       </div>
     </div>
