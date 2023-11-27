6f74ff0fb667649ee4e5e901277b87fa6b91ea4a
Alexsandr3 <fam_86@mail.ru>
2023-11-27 13:35:37 +0100

    feat(dashboard) fixed mistake

--- src/modules/contacts-module/components/contacts.tsx
+++ src/modules/contacts-module/components/contacts.tsx
@@ -70,7 +70,7 @@ export const Contacts: FC = () => {
         about: contacts.about,
       });
       setSelectedContacts(contacts);
-      setAboutText(contacts.about);
+      setAboutText(contacts.about || '');
     }
   }, [contacts]);
 
