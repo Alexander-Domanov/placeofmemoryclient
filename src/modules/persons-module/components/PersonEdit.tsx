4e1f6c14bc6335b659262b5db8d107fa084f907c
Alexsandr3 <fam_86@mail.ru>
2023-11-30 11:19:36 +0100

    feat(dashboard) added translate for status

--- src/modules/persons-module/components/PersonEdit.tsx
+++ src/modules/persons-module/components/PersonEdit.tsx
@@ -26,6 +26,7 @@ import {
   ILocation,
   IPersonById,
   Role,
+  Statuses,
 } from '@/types';
 import { routes } from '@/common/routing/routes';
 import { usePerson } from '@/modules/persons-module/hooks/usePerson';
@@ -266,6 +267,18 @@ export const PersonEdit: FC = () => {
   };
 
   const updateOptions = GetUpdateOptions(t, me);
+
+  if (
+    status === Statuses.PUBLISHED ||
+    me?.role === Role.USER ||
+    me?.role === Role.AUTHOR
+  ) {
+    updateOptions.push(
+      <Select.Option key={Statuses.PUBLISHED} value={Statuses.PUBLISHED}>
+        {t.dashboard.updateStatus.published}
+      </Select.Option>
+    );
+  }
 
   const isDisabled = GetDisabledStatus(status, me?.role as Role);
 
