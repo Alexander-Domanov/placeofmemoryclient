382733eb67236d5ff8403b37c20ec72f07c56e36
Alexsandr3 <fam_86@mail.ru>
2023-12-01 14:29:05 +0100

    feat(dashboard) corrected code

--- src/modules/places-module/hooks/useUpdatePlaceStatus.ts
+++ src/modules/places-module/hooks/useUpdatePlaceStatus.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updatePlaceStatus } from '@/modules/places-module/api/places-api';
 import { IResponseError } from '@/types/response-error-message.type';
 import { ErrorNotification } from '@/common-dashboard/errorNotification';
+import { noRefetch } from '@/common/helpers/noRefetch';
 
 export const useUpdatePlaceStatus = () => {
   const client = useQueryClient();
