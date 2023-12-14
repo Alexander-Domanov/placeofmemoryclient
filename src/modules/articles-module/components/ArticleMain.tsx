2ba438aa70ee849bdbe489f54e6326b272f5891e
Alexsandr3 <fam_86@mail.ru>
2023-12-14 19:08:07 +0100

    feat(dashboard) correct code

--- src/modules/articles-module/components/ArticleMain.tsx
+++ src/modules/articles-module/components/ArticleMain.tsx
@@ -49,7 +49,7 @@ export const ArticleMain: FC<Props> = ({ post }) => {
         </div>
 
         <Link
-          href={routes.articles.index}
+          href={routes.articles.page(Number(1))}
           className="mt-16 inline-flex text-5xl sm:text-4xl text-dark-500 rounded-full shadow-icon hover:bg-dark-200"
         >
           <BsArrowLeftCircleFill className="text-dark-300" />
